2ee022a608d6efe63bad69b4253b2631fe48ff66
Shovik Biswas <shovik.biswas@gmail.com>
2023-06-10 03:22:44 -0400

    Mobile home page

---
+++ index.js
@@ -0,0 +1,5 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+    
+    document.getElementById('navList').style.display = document.getElementById('navList').style.display =='grid' ?   'none' : 'grid';
+}
