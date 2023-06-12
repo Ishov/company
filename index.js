2c1c6e1370ab8ca3ee52bbe4b9b63d183e00314b
Shovik Biswas <shovik.biswas@gmail.com>
2023-06-12 00:09:51 -0400

    after lot of trouble

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ function myFunction(x) {
     x.classList.toggle("change");
     
     document.getElementById('navList').style.display = document.getElementById('navList').style.display =='grid' ?   'none' : 'grid';
+
 }
